96af7e344a73402fec25476aba9be1e4496a01cd
Elie <moussaelie497@gmail.com>
2024-05-01 13:01:24 +0300

    new patch fix the cursol dot and disable them adding pre -loder

--- js/main.js
+++ js/main.js
@@ -13,32 +13,46 @@
 	fullHeight();
 
 	var carousel = function() {
-		$('.featured-carousel').owlCarousel({
-	    loop: false,
-	    autoplay: false,
-		autoplayTimeout: 9999999,
-	    margin:30,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    dots: true,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:2
-	      },
-	      1000:{
-	        items:3
-	      }
-	    }
+		var $carousel = $('.featured-carousel');
+		$carousel.owlCarousel({
+			loop: false,
+			autoplay: false,
+			autoplayTimeout: 9999999,
+			margin: 30,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+			nav: false,
+			dots: true,
+			autoplayHoverPause: false,
+			items: 1,
+			navText: ["<span class='ion-ios-arrow-back'></span>", "<span class='ion-ios-arrow-forward'></span>"],
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 2
+				},
+				1000: {
+					items: 3
+				}
+			}
+		});
+	
+		// Add event listener to disable dots when needed
+		$carousel.on('disableDots', function() {
+			$carousel.addClass('disabled-dots');
+		});
+	
+		$carousel.on('enableDots', function() {
+			$carousel.removeClass('disabled-dots');
 		});
 
+		    // Disable dots after carousel initialization
+			$carousel.trigger('disableDots');
 	};
+	
 	carousel();
+	
 
 })(jQuery);
