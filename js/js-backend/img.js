c94f6e08c9f8320a3d97175f2c20f55a2438eb7f
Elie <moussaelie497@gmail.com>
2024-06-10 20:22:13 +0300

    testing

--- js/js-backend/img.js
+++ js/js-backend/img.js
@@ -38,7 +38,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
             document.getElementById('button').style.display = 'none'; 
 
             // Manually set cookies using JavaScript
-            const cookieExpiryTime = 60 * 1000; // Expiry time in milliseconds (1 minute)
+            const cookieExpiryTime = 120 * 1000; // Expiry time in milliseconds (1 minute)
             let cookieCreationTime = Date.now(); // Current timestamp in milliseconds
             document.cookie = `cookieCreationTime=${cookieCreationTime}; path=/; max-age=${cookieExpiryTime / 1000}; secure; samesite=None`;
             document.cookie = `uploadCount=${result.uploadCount}; path=/; max-age=${cookieExpiryTime / 1000}; secure; samesite=None`;
