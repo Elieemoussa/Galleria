77e8976b6d60aed30e83bd4b1f884f45eda02430
Elie <moussaelie497@gmail.com>
2024-06-24 15:49:29 +0300

    testing

--- js/js-backend/img.js
+++ js/js-backend/img.js
@@ -17,7 +17,7 @@ document.getElementById('uploadForm').addEventListener('submit', async function(
     formData.append('wedId', wedId);
 
     try {
-        const response = await fetch('https://wedcam-eb80ccd082f6.herokuapp.com/api/v1/img/uploadimg', { 
+        const response = await fetch('/api/v1/img/uploadimg', { 
             method: 'POST',
             body: formData,
             mode: 'cors',
