9bb1fcc7227cb54af4242abcacf8de63f81e85ea
Elie <moussaelie497@gmail.com>
2024-06-27 17:26:46 +0300

    adding loader for images

--- js/js-backend/img.js
+++ js/js-backend/img.js
@@ -104,6 +104,29 @@ document.getElementById('image').addEventListener('change', function() {
 });
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    var images = document.querySelectorAll('.scroll-container img');
+    var loader = document.getElementById('img');
+    var totalImages = images.length;
+    var imagesLoaded = 0;
+
+    images.forEach(function(image) {
+        image.addEventListener('load', function() {
+            imagesLoaded++;
+            if (imagesLoaded === totalImages) {
+                loader.style.display = 'none';
+                document.querySelectorAll('.image-container').forEach(function(container) {
+                    container.classList.remove('hidden');
+                });
+            }
+        });
+
+        // In case images are cached
+        if (image.complete) {
+            image.dispatchEvent(new Event('load'));
+        }
+    });
+});
 
 
 
